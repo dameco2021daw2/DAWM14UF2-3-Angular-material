897fc9c5537303d9db48dd9719b88196953ca614
sergi grau <sergi.grau@fje.edu>
2017-12-07 21:02:29 +0100

    nova interfície

--- src/app/m11-encaminament.component.ts
+++ src/app/m11-encaminament.component.ts
@@ -19,11 +19,19 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'aplicacio',
   template: `
-    <h1>{{titol}}</h1>
-    <nav>
-      <a routerLink="/salutacio" routerLinkActive="active">Salutacio</a>
-      <a routerLink="/suma" routerLinkActive="active">Suma</a>
-    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">{{titol}}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" routerLink="/salutacio" routerLinkActive="active">Salutacio </a>
+        <a class="nav-item nav-link" routerLink="/suma" routerLinkActive="active">Suma</a>
+        
+      </div>
+    </div>
+  </nav>
     <router-outlet></router-outlet>
   `
 })
